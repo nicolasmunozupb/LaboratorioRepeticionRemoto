5e7fa6e19769423f45f5a16cbff5557fbb2b8222
Munoz Vega <000400505@bga.upb>
2019-03-01 13:59:34 -0500

    “ejercicio resuelto

--- mayor.py
+++ mayor.py
@@ -1,7 +1,19 @@
-a=int(input("ingrese un valor"))
+mayores=0
+cero=0
+menores=0
 
-if a>0:
-    print("el numero es mayor que cero")
-else:
-    print("el numero es menor que cero")
-    
+for x in range(0,10):
+    x=int(input("digite un numero"))
+    if x==0:
+            cero=cero + 1
+
+    else:
+        if x>0:
+            mayores=mayores + 1
+
+        else:
+            menores=menores + 1
+
+print("la cantidad de cero es: ", cero)
+print("la cantidad de mayores es: ", mayores)
+print("la cantidad de cero es: ", menores)
