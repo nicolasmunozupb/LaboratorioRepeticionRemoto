cfa4e4970d2cc0b1629364adf083e18e2a28ebef
Munoz Vega <000400505@bga.upb>
2019-03-01 14:08:00 -0500

    “creacion segundo algoritmo

---
+++ positivos.py
@@ -0,0 +1,8 @@
+n1=int(input("ingrese valor n1"))
+if n1==0:
+	print("el numero es 0")
+else:
+        if n1%2==0:
+                print("su numero es par")
+        else:
+                print("su numero no es par")
